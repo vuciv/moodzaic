47e4ae2ddc7be90efb7ee87cf5ae084d84aad2c3
Molly Fortnow <mfortnow@MoFos-MaBo.local>
2019-11-03 20:34:10 -0600

    figuring out testing framework

--- user-interface/moodzaic/src/App.test.js
+++ user-interface/moodzaic/src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hey, stranger");
+
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Margaret!");
 });
