744b87134461d2a9663dfe6d7c15b587972fe02b
esohlberg <esohlberg@uchicago.edu>
2019-11-05 17:29:03 -0600

    basic user tests finished

--- moodzaic_django/users/models.py
+++ moodzaic_django/users/models.py
@@ -2,14 +2,40 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
     age = models.IntegerField(default=18)
     gender = models.CharField(max_length=9)
 
-    def setUsername(self, username):
-        self.username = username
+    def setUserUsername(self, username):
+        #TODO
+        return 'not false'
+    
+    def setUserPassword(self, password):
+        #TODO
+        return 'not false'
+    
+    def setUserAge(self, age):
+        #TODO
+        return 'not false'
+    
+    def setUserGender(self, age):
+        #TODO
+        return 'not false'
+
+class Goal(models.Model):
+    goal = models.CharField(max_length=30)
+    frequency = models.IntegerField(default=1)
+    time = models.TimeField()
+
+    def setGoalGoal(self, goal):
+        #TODO
         return
     
-    def getUsername(self):
-        return self.username
+    def setGoalFrequency(self, frequency):
+        #TODO
+        return
+    
+    def setGoalTime(self, time):
+        #TODO
+        return
--- moodzaic_django/users/tests.py
+++ moodzaic_django/users/tests.py
@@ -1,12 +1,61 @@
 from django.test import TestCase
-from users.models import User
+from users.models import User, Goal
 
 # Create your tests here.
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(username = "emil")
-    def test_setUsername(self):
+        User.objects.create(username = "emil", password = "snibby")
+
+    def test_setUsernameSuccess(self):
         testUser = User.objects.get(username = "emil")
-        testUser.setUsername("emil1")
-        self.assertEqual("emil1", testUser.getUsername())
+        testUser.setUserUsername("emil1")
+        self.assertEqual("emil1", testUser.username)
+    def test_setUsernameLengthFailure(self):
+        testUser = User.objects.get(username = "emil")
+        self.assertFalse(testUser.setUserUsername("123456789012345678901"))
+
+    def test_setUserPasswordSuccess(self):
+        testUser = User.objects.get(username = "emil")
+        testUser.setUserPassword("snibby2")
+        self.assertEqual("snibby2", testUser.password)
+    def test_setUserPasswordTooLongFailure(self):
+        testUser = User.objects.get(username = "emil")
+        self.assertFalse(testUser.setUserPassword("123456789012345678901"))
+    def test_setUserPasswordTooShortFailure(self):
+        testUser = User.objects.get(username = "emil")
+        self.assertFalse(testUser.setUserPassword("1234567"))
+
+    def test_setUserAgeSuccess(self):
+        testUser = User.objects.get(username = "emil")
+        testUser.setUserAge(21)
+        self.assertEqual(21, testUser.age)
+    def test_setUserAgeTooYoungFailure(self):
+        testUser = User.objects.get(username = "emil")
+        self.assertFalse(testUser.setUserAge(17))
+
+    def test_setUserGenderManSuccess(self):
+        testUser = User.objects.get(username = "emil")
+        testUser.setUserGender('man')
+        self.assertEqual('man', testUser.gender)
+    def test_setUserGenderWomanSuccess(self):
+        testUser = User.objects.get(username = "emil")
+        testUser.setUserGender('woman')
+        self.assertEqual('woman', testUser.gender)
+    def test_setUserGenderNonbinarySuccess(self):
+        testUser = User.objects.get(username = "emil")
+        testUser.setUserGender('nonbinary')
+        self.assertEqual('nonbinary', testUser.gender)
+    def test_setUserGenderFailure(self):
+        testUser = User.objects.get(username = "emil")
+        self.assertFalse(testUser.setUserGender('gibberish'))
+
+
+class GoalTestCase(TestCase):
+    def setUp(self):
+        Goal.objects.create(goal = "Drink water", frequency = "5", time = "16:00 AM")
+    
+    def test_setGoalGoal(self):
+        testGoal = Goal.objects.get(goal = "Drink water")
+        testGoal.setGoalGoal("Drink more water")
+        self.assertEqual("Drink more water", testGoal.goal)
