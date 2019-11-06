a5eb1dd180abf51d919c76f92676e117ccf5373b
jfonseca8 <fonseca@uchicago.edu>
2019-11-05 18:13:09 -0600

    added some functions for community

---
+++ moodzaic_django/community/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Community(models.Model):
+    name = models.CharField(max_length=30)
+    users = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+    def setName(self, name):
+        self.name = name
+        return
+
+    def getName(self):
+        return self.name
+
+    def setUsers(self, users):
+        self.users = users
+        return
+
+    def getUsers(self):
+        return self.users
+
+    def addUserToCommunity(community, user):
+        community.users.add(user)
+        return
+
+
+
+
+
--- moodzaic_django/moodzaic_django/settings.py
+++ moodzaic_django/moodzaic_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'community.apps.CommunityConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
