9f6ccfe8f7476986ee208df224986d3a71404358
jfonseca8 <fonseca@uchicago.edu>
2019-11-05 18:25:02 -0600

    tests correctly run

--- moodzaic_django/community/tests.py
+++ moodzaic_django/community/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
 from community.models import Community
-from users.models import Community
+from users.models import User
 
 # Create your tests here.
 
 class CommunityTestCase(TestCase):
-    
+
     def setUp(self):
         Community.objects.create(name = "fitness")
 
