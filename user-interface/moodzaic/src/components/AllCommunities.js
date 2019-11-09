a4c7e794b55e999ceed84cb4565845839981cff3
Molly Fortnow <mfortnow@MoFos-MaBo.local>
2019-11-09 16:58:50 -0600

    wrote allcommunities, communitypage, and mycommunities

--- user-interface/moodzaic/src/components/AllCommunities.js
+++ user-interface/moodzaic/src/components/AllCommunities.js
@@ -15,14 +15,34 @@ import {
 import MyMenu from './Menu.js';
 import Footer from './Footer.js';
 
-const AllCommunities = ({myCommunities}) => (
-  <div>
-    <MyMenu />
-    <Container text style={{ marginTop: '7em' }}>
-    <p> should show all the communities and allow you to select any to join</p>
-    </Container>
-    <Footer />
-  </div>
-)
+function addCommunity(c) {
+  //add the community c to the list of all communities in the overall data
+  //unless it's already added lol
+}
+
+function isMyCommunity(c) {
+  //check if the community c is in your personal list of communities
+}
+
+function AllCommunities({allCommunities}) {
+  const communities = allCommunities.map((com, i) => {
+    return <Button
+              color='teal' fluid size='large'
+              key = {i}
+              onClick = {addCommunity(com)}>
+              {com}: {isMyCommunity(com) ? 'added!' : 'add?'}
+            </ Button>;
+  })
+
+  return (
+    <div>
+      <Container text style={{ marginTop: '7em' }}>
+        <p> should show all the communities and allow you to select any to join</p>
+        {communities}
+      </Container>
+    </div>
+  )
+}
+
 
 export default AllCommunities
--- user-interface/moodzaic/src/components/CommunityPage.js
+++ user-interface/moodzaic/src/components/CommunityPage.js
@@ -18,19 +18,87 @@ import Community from './Community.js'
 import CommunitiesPage from './MyCommunities.js'
 import AllCommunities from './AllCommunities.js'
 
+const getMyCommunityList = (username) => {
+  //get the community list of the username from the data
+  return;
+}
+
+const getAllCommunities = () => {
+  //get the info of all the communities
+  return;
+}
+
+class CommunityPage extends React.Component {
+  state = {
+        Community: '',
+        AddMode: false,
+        MyCommunityList: getMyCommunityList(this.props.username),
+        CommunityList: getAllCommunities()
+        // should these be props?
+  }
+
+  toggleAddMode = () => {
+    this.setState(prevState => ({
+      LoggedIn: !prevState.LoggedIn
+    }))
+  }
+
+  OpenCommunity = (acommunity) => {
+    this.setState(prevState => ({
+      Community: acommunity
+    }))
+  }
 
 
-const CommunityPage = ({myCommunities}) => (
-  <div>
-    <MyMenu />
-    <Container text style={{ marginTop: '7em' }}>
-    <p> should call my communities
-    unless:
-    user chooses to see all communities, then calls AllCommunities
-    or a community is selected, then call community and open that community</p>
-    </Container>
-    <Footer />
-  </div>
-)
 
-export default CommunityPage
+  render() {
+    const addMode = this.state.AddMode;
+    const community = this.state.Community;
+    const myCommunityList = this.state.MyCommunityList;
+    const communityList = this.state.CommunityList;
+    let myPage, myButton;
+
+    if (community !== '') {
+      myPage = <Community myCommunity = {community} />;
+      myButton =
+      <Button color='teal' fluid size='large' onClick = {this.toggleCommunity('')}>
+        See My Communities
+      </Button>;
+    }
+
+    else if (addMode === false) {
+      myPage = <CommunitiesPage communityCallback = {this.OpenCommunity} myCommunities = {myCommunityList}/>;
+      myButton =
+      <Button color='teal' fluid size='large' onClick = {this.toggleAddMode}>
+        See All Communities
+      </Button>;
+    }
+
+    else {
+      myPage = <AllCommunities allCommunities = {communityList}/>;
+      myButton =
+      <Button color='teal' fluid size='large' onClick = {this.toggleAddMode}>
+        See My Communities
+      </Button>;
+    }
+
+    return (
+      <div>
+        <MyMenu />
+        <Container text style={{ marginTop: '7em' }}>
+          <p> should call my communities
+          unless:
+          user chooses to see all communities, then calls AllCommunities
+          or a community is selected, then call community and open that community</p>
+          {myPage}
+          {myButton}
+        </Container>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+
+
+export default CommunityPage;
