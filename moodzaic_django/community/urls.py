8fee53514cfcc77b8051e9e5759e37612639a2bf
jfonseca8 <fonseca@uchicago.edu>
2019-11-13 17:23:20 -0600

    Community pages exist! Yay!

--- moodzaic_django/community/urls.py
+++ moodzaic_django/community/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('api/community/', views.CommunityListCreate.as_view()),
+    # path('api/community/', views.CommunityListCreate.as_view()),
+    url(r'^api/community/(?P<name>[\w]+)$', views.communityDetails),
 ]
--- moodzaic_django/community/views.py
+++ moodzaic_django/community/views.py
@@ -1,7 +1,39 @@
 from community.models import Community
 from community.serializers import CommunitySerializer
-from rest_framework import generics
 
-class CommunityListCreate(generics.ListCreateAPIView):
-    queryset = Community.objects.all()
-    serializer_class = CommunitySerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+# class CommunityListCreate(generics.ListCreateAPIView):
+#     queryset = Community.objects.all()
+#     serializer_class = CommunitySerializer
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def communityDetails(request, name):
+    """
+    Retrieve, update or get a community by name.
+    """
+    try:
+        community = Community.objects.get(name=name)
+        serializer = CommunitySerializer(community,context={'request': request})
+        return Response(serializer.data)
+    except Community.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'GET':
+            serializer = CommunitySerializer(community,context={'request': request})
+            return Response(serializer.data)
+
+            # elif request.method == 'PUT':
+            #     serializer = CustomerSerializer(customer, data=request.data,context={'request': request})
+            #     if serializer.is_valid():
+            #         serializer.save()
+            #         return Response(serializer.data)
+            #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == 'DELETE':
+            community.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
