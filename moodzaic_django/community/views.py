82c0a374a25dfdd4cc19c1868b0e7c7e12f2734d
jfonseca8 <fonseca@uchicago.edu>
2019-11-14 01:59:19 -0600

    merge fix

--- moodzaic_django/community/views.py
+++ moodzaic_django/community/views.py
@@ -6,21 +6,18 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-<<<<<<< HEAD
+
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
-=======
 import logging
 
 logger = logging.getLogger(__name__)
 
 
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
-
 # class CommunityListCreate(generics.ListCreateAPIView):
 #     queryset = Community.objects.all()
 #     serializer_class = CommunitySerializer
 
-<<<<<<< HEAD
+
 @csrf_exempt
 @api_view(['GET', 'PUT', 'DELETE'])
 @ensure_csrf_cookie
@@ -32,7 +29,7 @@ def comDet(request):
 @csrf_exempt
 @api_view(['GET', 'POST', 'DELETE'])
 @ensure_csrf_cookie
-=======
+
 @api_view(['POST'])
 def makeCommunity(request):
     if request.method == 'POST':
@@ -51,7 +48,6 @@ class CommunityListCreate(generics.ListCreateAPIView):
     serializer_class = CommunitySerializer
 
 @api_view(['GET', 'POST', 'DELETE'])
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
 def communityDetails(request, name):
     """
     Retrieve, update or get a community by name.
@@ -67,7 +63,6 @@ def communityDetails(request, name):
             serializer = CommunitySerializer(community,context={'request': request})
             return Response(serializer.data)
 
-<<<<<<< HEAD
         elif request.method == 'POST':
         #    serializer = CommunitySerializer(community, data=request.data,context={'request': request})
         #    if serializer.is_valid():
@@ -79,7 +74,6 @@ def communityDetails(request, name):
         elif request.method == 'DELETE':
             community.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-=======
         # elif request.method == 'POST':
         #         serializer = CustomerSerializer(customer, data=request.data,context={'request': request})
         #         if serializer.is_valid():
@@ -90,4 +84,3 @@ def communityDetails(request, name):
         # elif request.method == 'DELETE':
         #     community.delete()
         #     return Response(status=status.HTTP_204_NO_CONTENT)
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
--- moodzaic_django/frontend/src/integration_funcs.js
+++ moodzaic_django/frontend/src/integration_funcs.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const API_URL = 'localhost:8000/api/';
+const API_URL = 'http://localhost:8000/api/';
 
-<<<<<<< HEAD
-export function addCommunity(c) {
-  // return axios.post(`${API_URL}community/`, c)
-  //   .then(response => {
-  //     console.log(response);
-  //     console.log(response.data);
-  //   })
-  //   .catch(error => console.log(error))
-    //add the community c to the list of all communities in the overall data
-    //unless it's already added lol
-  }
+  export function createCommunity(c) {
+      //add the community c to the list of all communities in the overall data
+      //unless it's already added lol
+    }
+
+    export function updateCommunity(c) {
+        //add the community c to the list of all communities in the overall data
+        //unless it's already added lol
+      }
 
   export function isMyCommunity(c) {
     //check if the community c is in your personal list of communities
@@ -23,48 +21,23 @@ export function addCommunity(c) {
     return [];
   }
 
+  //get the info of all the communities
   export const getAllCommunities = () => {
-    //get the info of all the communities
-    return [];
+    return axios.get(`${API_URL}community/all`)
   }
-=======
-// export function addCommunity(c) {
-//     //add the community c to the list of all communities in the overall data
-//     //unless it's already added lol
-//   }
-//
-//   export function isMyCommunity(c) {
-//     //check if the community c is in your personal list of communities
-//   }
-//
-//   export const getMyCommunityList = (username) => {
-//     //get the community list of the username from the data
-//     return [];
-//   }
-//
-//   export const getAllCommunities = () => {
-//     //get the info of all the communities
-//     return [];
-//   }
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
 
   function getUsers() {
-    return axios.get(`${API_URL}community/all`).then(response => response.data).catch(error => console.log(error));
+    return axios.get(`${API_URL}users`).then(response => response.data).catch(error => console.log(error));
   }
 
-<<<<<<< HEAD
   export function createUser(){
-=======
-<<<<<<< HEAD
-  export function createUser(){
-    return axios.post(`${API_URL}community/`)
-=======
-  export function createUser(community){
->>>>>>> 4be427057d922b12ece3d1f4dd6d53cf8d5af624
     return axios.post(`${API_URL}community/`, {
-      name: "jerseysCommunity"
+      name: "jerseysCommunity",
+      users: [{
+        username: "jerseyFonseca",
+        password: "secretsecret"
+      }]
     })
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
       .then(response => {
         console.log(response);
         console.log(response.data);
@@ -81,45 +54,24 @@ export function addCommunity(c) {
 
   }
 
-  export function getAllCommunities() {
-      const url = `${API_URL}/community/`;
-      return axios.get(url).then(response => response.data);
-  }
-  export function getCommunity(name) {
-      const url = `${API_URL}$/community/{name}`;
-      return axios.get(url).then(response => response.data);
-  }
-  // export function deleteCommunity(community){
-  //     const url = `${API_URL}$/community/{community.name}`;
-  //     return axios.delete(url);
-  // }
-  export function createCommunity(community){
-      const url = `${API_URL}/community/`;
-      return axios.post(url,community);
-  }
-  export function updateCommunity(community){
-      const url = `${API_URL}/community/${community.name}`;
-      return axios.put(url,community);
+
+  export const getPosts = (com) => {
+    //get a list of all the posts from the community
+    const ret = [];
+    // for each in com.posts {
+    //   ret.append({
+    //     poster: post.poster lol so like this is not in the class diagram
+    //     message: post.post,
+    //     time: post.post_time,
+    //     comment_list: post.comment_list (list of objects: poster (string) and message (string))
+    //   })
+    // }
+    return ret;
   }
 
-  //
-  // export const getPosts = (com) => {
-  //   //get a list of all the posts from the community
-  //   const ret = [];
-  //   // for each in com.posts {
-  //   //   ret.append({
-  //   //     poster: post.poster lol so like this is not in the class diagram
-  //   //     message: post.post,
-  //   //     time: post.post_time,
-  //   //     comment_list: post.comment_list (list of objects: poster (string) and message (string))
-  //   //   })
-  //   // }
-  //   return ret;
-  // }
-  //
-  // export const sendPost = (post) => {
-  //   //add a post to the backend
-  //   //complete with poster, message, time, and comment_list
-  //   //adds it to the correct list -- the og list of posts or whatever comment post or whatever
-  //   return;
-  // }
+  export const sendPost = (post) => {
+    //add a post to the backend
+    //complete with poster, message, time, and comment_list
+    //adds it to the correct list -- the og list of posts or whatever comment post or whatever
+    return;
+  }
