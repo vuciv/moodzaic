919000200899c120c59a7091bad72f279c651b6d
Zipporah Klain <zklain@uchicago.edu>
2019-11-14 02:24:32 -0600

    fix git lines

--- moodzaic_django/community/urls.py
+++ moodzaic_django/community/urls.py
@@ -4,13 +4,6 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-<<<<<<< HEAD
-    # path('api/community/', views.CommunityListCreate.as_view()),
-    path('api/community/', views.comDet),
-    url(r'^api/community/(?P<name>[\w]+)$', csrf_exempt(views.communityDetails))
-]
-=======
     path('api/community/', views.makeCommunity),
     url(r'^api/community/(?P<name>[\w]+)$', views.communityDetails),
 ]
->>>>>>> 76b65dfb6043e2b7e2258cb24249f0c89f024fd5
