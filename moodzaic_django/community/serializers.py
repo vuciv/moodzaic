18aebfa4514313c82630eb6aff822ba852d95901
jfonseca8 <fonseca@uchicago.edu>
2019-11-17 15:31:06 -0600

    communities now have users

--- moodzaic_django/community/serializers.py
+++ moodzaic_django/community/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from community.models import Community
-from users.models import User
+from users.serializers import UserSerializer
 
 class CommunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
-        exclude = ['users']
+        users = UserSerializer(many=True)
+        fields = '__all__'
